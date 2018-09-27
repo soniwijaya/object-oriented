13384506500072551810763eec4d71b833cdad45
Soni Wijaya <soni@MMDCs-MacBook-Air.local>
2018-09-27 23:12:48 +0700

    basic konsep dari object oriented buat property class dan instance class

---
+++ Coba.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: soni
+ * Date: 27/09/18
+ * Time: 23.11
+ */
+
+class Coba
+{
+    public $a; //property
+
+    //method
+    public function b()
+    {
+
+    }
+}
+
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
